c8d926d73722bf5e132cfbaa9cf06b3485679a75
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-10-21 00:46:28 +0200

    restore userDetailsService method in SecurityConfiguration for in-memory user management

--- Backend/events/src/main/java/rs/ac/ftn/svt/events/configuration/SecurityConfiguration.java
+++ Backend/events/src/main/java/rs/ac/ftn/svt/events/configuration/SecurityConfiguration.java
@@ -11,8 +11,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -36,24 +39,24 @@ public class SecurityConfiguration {
     }
 
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//
-//        UserDetails user1 = User
-//                .withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("admin")
-//                .roles("ADMIN")
-//                .build();
-//
-//        UserDetails user2 = User
-//                .withDefaultPasswordEncoder()
-//                .username("pera")
-//                .password("pera123")
-//                .roles("USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user1, user2);
-//    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("pera")
+                .password("pera123")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
