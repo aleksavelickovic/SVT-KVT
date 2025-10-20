20df9cafb64925a83788a0a8d0d653a303a87d11
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-10-20 21:27:24 +0200

    initialize Spring Boot project with Maven configuration and basic structure

---
+++ Backend/events/src/test/java/rs/ac/ftn/svt/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.svt.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
