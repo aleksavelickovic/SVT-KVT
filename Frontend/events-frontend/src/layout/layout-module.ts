67ff29b48c0879883085df51e2c20568cbc1c503
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-10-21 14:33:22 +0200

    feat: add authentication and layout modules with NavBar component

---
+++ Frontend/events-frontend/src/layout/layout-module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavBar} from './nav-bar/nav-bar';
+
+
+@NgModule({
+  declarations: [
+    NavBar
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule {
+}
