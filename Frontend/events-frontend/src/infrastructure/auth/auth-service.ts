0f7b9b3a56a9839d1cfb6ddbdc41504550259235
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-10-21 14:46:32 +0200

    feat: implement authentication role management in AuthService

--- Frontend/events-frontend/src/infrastructure/auth/auth-service.ts
+++ Frontend/events-frontend/src/infrastructure/auth/auth-service.ts
@@ -1,8 +1,41 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {JwtHelperService} from '@auth0/angular-jwt';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  user$: BehaviorSubject<string> = new BehaviorSubject("");
+  userState: Observable<string> = this.user$.asObservable();
+  private headers: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+    skip: 'true',
+  });
+
+  constructor(private http: HttpClient) {
+    this.user$.next(this.getRole());
+  }
+
+
+  getRole(): any {
+    if (this.isLoggedIn()) {
+      const accesToken: any = localStorage.getItem('user');
+      const helper = new JwtHelperService();
+      return helper.decodeToken(accesToken).role[0].authority
+    }
+    return null;
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem('user') != null;
+  }
+
+  setUser(): void {
+    this.user$.next(this.getRole());
+  }
 
 }
